5690db905ec5b8a97f4f186028c904e06a938e62
Sweets Sweetman <sweetmantech@gmail.com>
2025-03-22 17:14:17 -0300

    refactor to fix build errors.

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,28 +1,18 @@
 import { anthropic } from "@ai-sdk/anthropic";
 import { experimental_createMCPClient, streamText } from "ai";
-import { Experimental_StdioMCPTransport } from "ai/mcp-stdio";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  //   // Initialize an MCP client to connect to a `stdio` MCP server:
-  //   const transport = new Experimental_StdioMCPTransport({
-  //     command: "node",
-  //     args: ["mcp/web-search/build/index.js"],
-  //   });
-  //   const clientOne = await experimental_createMCPClient({
-  //     transport,
-  //   });
-  // Alternatively, you can connect to a Server-Sent Events (SSE) MCP server:
-  const clientTwo = await experimental_createMCPClient({
+  const braveWebSearchMcpClient = await experimental_createMCPClient({
     transport: {
       type: "sse",
       url: "https://router.mcp.so/sse/bs1hevm8kn8t98",
     },
   });
-  const toolSetOne = await clientTwo.tools();
+  const toolSetWebSearch = await braveWebSearchMcpClient.tools();
   const tools = {
-    ...toolSetOne,
+    ...toolSetWebSearch,
   };
 
   const result = streamText({
