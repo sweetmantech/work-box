a0d2235c15ce7bcce6a826c117a54fe769a92810
Sweets Sweetman <sweetmantech@gmail.com>
2025-03-22 17:12:26 -0300

    Search MCP server    actual: no websearch tool    rewuired: add websearch tool

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,29 @@
 import { anthropic } from "@ai-sdk/anthropic";
-import { streamText } from "ai";
+import { experimental_createMCPClient, streamText } from "ai";
+import { Experimental_StdioMCPTransport } from "ai/mcp-stdio";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
+
+  //   // Initialize an MCP client to connect to a `stdio` MCP server:
+  //   const transport = new Experimental_StdioMCPTransport({
+  //     command: "node",
+  //     args: ["mcp/web-search/build/index.js"],
+  //   });
+  //   const clientOne = await experimental_createMCPClient({
+  //     transport,
+  //   });
+  // Alternatively, you can connect to a Server-Sent Events (SSE) MCP server:
+  const clientTwo = await experimental_createMCPClient({
+    transport: {
+      type: "sse",
+      url: "https://router.mcp.so/sse/bs1hevm8kn8t98",
+    },
+  });
+  const toolSetOne = await clientTwo.tools();
+  const tools = {
+    ...toolSetOne,
+  };
 
   const result = streamText({
     model: anthropic("claude-3-7-sonnet-20250219"),
@@ -12,6 +33,7 @@ export async function POST(req: Request) {
         thinking: { type: "enabled", budgetTokens: 12000 },
       },
     },
+    tools,
   });
 
   return result.toDataStreamResponse({
